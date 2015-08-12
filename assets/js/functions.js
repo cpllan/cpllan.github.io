fb1f888e70014099ae08444583e06835f5d45aab
Alex Capellan <alecapellan@me.com>
2015-08-11 21:46:57 -0500

    Sergios

---
+++ assets/js/functions.js
@@ -0,0 +1,27 @@
+// @codekit-prepend "owl.carousel.min.js";
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1,
+              nav:false,
+              dots:true,
+          },
+          600:{
+              items:2,
+              nav:false,
+              dots:true,
+              slideBy:2
+          },
+          1000:{
+              items:3,
+              nav:false,
+              dots:true,
+              slideBy:3
+          }
+      }
+  });
+});
