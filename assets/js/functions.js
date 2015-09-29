4d280bae75cb976dabd570a165427d9917b3746e
Alex Capellan <alecapellan@me.com>
2015-09-29 18:52:21 -0500

    Portfolio

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,37 +1,5 @@
-// @codekit-prepend "owl.carousel.min.js";
-// @codekit-prepend "slidebars.min.js";
+$( document ).ready(function() {
 
-$(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-      loop:true,
-      margin:10,
-      responsiveClass:true,
-      responsive:{
-          0:{
-              items:1,
-              nav:false,
-              dots:true,
-          },
-          600:{
-              items:2,
-              nav:false,
-              dots:true,
-              slideBy:2
-          },
-          900:{
-              items:3,
-              dots:true,
-              slideBy:3,
-              autoplay:true,
-              responsiveRefreshRate:200,
-              navigation:true
-          }
-      }
-  });
+  // Get started!
+
 });
-
-(function($) {
-  $(document).ready(function() {
-    $.slidebars();
-  });
-}) (jQuery);
